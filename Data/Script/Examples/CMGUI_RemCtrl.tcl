1913741abd7e1461cd045b04a0810b8090d05b8f
Keerthi SREERANGAIAH (FERCHAU Automotive) <keerthi.sreerangaiah@ferchau.com>
2024-04-23 13:45:23 +0200

    Files added

---
+++ Data/Script/Examples/CMGUI_RemCtrl.tcl
@@ -0,0 +1,92 @@
+# Set which CarMaker application to load
+Application exe "$env(CMAPP)"
+
+# Start this CarMaker application
+Application start
+
+# Set the saving mode
+SaveMode save_all
+
+#Set File format
+#DStore.Format = erg
+#IFileModify Data/Config/OutputQuantities DStore.Format erg
+#IFileFlush
+
+# If a TestSeries to simulate was specified
+if $env(SIM_TS) {
+	
+	# Load a TestSeries
+	LoadTestRun "$env(TSFPATH)"
+
+	# -> Can be triggered by below command too, just it 
+	# popups the TestMgr window too using LoadTestRun
+	# TestMgr load $env(TSFPATH)
+
+	# Start simulation of the TestSeries
+	TestMgr start
+	
+	# Set the path to the report folder
+    set report_folder "doc"
+
+	#Diagram setting small defined here
+	Report loadtemplate TestTemplate.cmrep
+
+	# Set a convenient name for the report file
+    set RptFPath [file join $report_folder "$env(TSFNAME)"]
+    set RptFPath [file rootname "$RptFPath"]
+    set RptFPath "${RptFPath}.pdf"
+	
+	# Let some time to finish to prepare the report diagrams
+	after 1000
+
+	# Generate the report for simulated TestSeries
+	Report create "$RptFPath"
+}
+
+# If a TestRun to simulate was specified
+if $env(SIM_TR) {
+	
+	# Load a TestRun
+	LoadTestRun "$env(TRFNAME)"
+
+	# Start simulation of the TestRun
+	StartSim
+	WaitForStatus running
+	WaitForStatus idle
+}
+
+# If a model check to simulate was specified
+if $env(SIM_MC) {
+	
+	# Load a TestRun
+	LoadTestRun "$env(MNAME)"
+
+	# Set Vehicle value
+	KeyValue set Vehicle "$env(VHCLPATH)"
+	
+	# open model check window
+	ModelCheck::Popup
+	# trigger model check calculations
+	ModelCheck::Run
+	
+	# Start simulation of the TestRun
+	StartSim
+	WaitForStatus running
+	WaitForStatus idle
+}
+
+# Reset the saving mode
+SaveMode collect_only
+
+# Close CarMaker application
+Application disconnect
+
+# If specified that APO broker daemon must be closed
+if $env(STOP_ABD) {
+
+	# Close APO broker daemon
+	exec "$env(APOBD)" -stop
+}
+
+# Close Carmaker GUI
+exit
